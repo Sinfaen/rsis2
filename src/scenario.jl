f4d4b29d1bf77150669f229829ea76cb24a8fc2e
Kelly Schultz <ksjaculus@gmail.com>
2023-11-17 02:26:10 -0500

    TEMP: add scheduling

--- src/scenario.jl
+++ src/scenario.jl
@@ -22,10 +22,30 @@ Base.show(io::IO, p::ProjectInfo) = print(io,
     :desc      = "$(p.desc)"
     """)
 
+struct ModelCallback
+    mod    :: String
+    freq   :: Int # relative to thread frequency
+    offset :: Int
+end
+
+mutable struct ThreadInfo
+    freq :: Float64
+    schedule :: Vector{ModelCallback}
+    function ThreadInfo()
+        new(1.0, Vector{ModelCallback}())
+    end
+    function ThreadInfo(f::Float64)
+        new(f, Vector{ModelCallback}())
+    end
+end
+
 mutable struct Scenario
     current_project :: ProjectInfo
+
+    paths :: Vector{String} # file path system
+    threads :: Vector{ThreadInfo}
     function Scenario()
-        new(ProjectInfo())
+        new(ProjectInfo(), Vector{String}(), Vector{ThreadInfo}())
     end
 end
 
@@ -51,8 +71,17 @@ function projectinfo() :: ProjectInfo
     return _scene.current_project
 end
 
-function setproject(p::ProjectInfo)
+function setproject(p::ProjectInfo) :: Nothing
     _scene.current_project = p
+    return
+end
+
+function addpath(p::String) :: Nothing
+    if p in _scene.paths
+        @warn "Path $(p) already registered"
+    else
+        push!(_scene.paths, p)
+    end
     return
 end
 
